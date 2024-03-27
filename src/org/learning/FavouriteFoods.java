15410a714088f982ac29f973528e82cd419e1cac
aSwanting <gab.damico@gmail.com>
2024-03-27 15:36:10 +0100

    add - middle food, formatting

--- src/org/learning/FavouriteFoods.java
+++ src/org/learning/FavouriteFoods.java
@@ -4,10 +4,13 @@ public class FavouriteFoods {
     public static void main(String[] args) {
         String[] foods = {"Pizza", "Burger", "Sushi", "Pasta", "Salad", "Tacos", "Ice Cream"};
         int foodNumber = foods.length;
-        String favFood = foods[0];
+        String topFavFood = foods[0];
+        String midFavFood = foods[foodNumber / 2];
         String leastFavFood = foods[foods.length - 1];
-
-        System.out.println("You have " + foodNumber + " food items in your list. Your favourite food is " + favFood + " and your least favourite is " + leastFavFood);
-
+        System.out.print(
+                "You have " + foodNumber + " food items in your list. Your top food is "
+                        + topFavFood + ", your middle is " + midFavFood
+                        + " and your least favourite is " + leastFavFood + "."
+        );
     }
 }
